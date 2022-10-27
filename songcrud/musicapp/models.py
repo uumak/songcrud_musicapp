61b33f565d301822b24a1b23ab912c4338aa8491
uumak <makanjuolayusuf@gmail.com>
2022-10-27 15:28:36 +0100

    updated project for week 5 ready to be pushed

---
+++ songcrud/musicapp/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from datetime  import datetime
+
+# Create your models here.
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    age = models.IntegerField()
+    
+    def __str__(self):
+        return self.first_name
+    
+class Song(models.Model):
+    title = models.CharField(max_length=70)
+    date_released = models.DateTimeField(auto_now_add=True)
+    likes = models.IntegerField()
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
+    
+class Lyric(models.Model):
+    content = models.CharField(max_length=1000)
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
+        
+    def __str__(self):
+        return self.content
